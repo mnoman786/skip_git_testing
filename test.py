44ad8c453538f716c25575a8aed6e5423ae783e3
Noman ch <mnomanch786@gmail.com>
2023-02-19 12:47:43 +0500

    first commit in new bracnh skipq_branch

--- test.py
+++ test.py
@@ -2,5 +2,7 @@ print("hy my name is noman ch and i am from pakistan")
 
 def ubuntu():
     print("hy i am from ubuntu")
+    print("i am using ubuntu 18.04")
+    print("i am using python 3.6")
     
 
